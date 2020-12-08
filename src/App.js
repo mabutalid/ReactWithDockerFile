654076737ea7a3afae70d6a311beff6e29f5f71c
marco-lambdas <marco@lambdas.io>
2020-12-08 19:44:22 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to a Dockerized React App</h1>
+        <h2>Placed in an AWS ECR</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
